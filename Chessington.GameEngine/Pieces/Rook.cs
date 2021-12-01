f202b1d2b65d6363c4b03883084f0ef9dfe6d0d0
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2021-12-01 17:04:02 +0000

    red-03

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -10,7 +10,15 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentsquare = board.FindPiece(this);
+            List<Square> moves = new List<Square>();
+            for (var i = 0; i < 8; i++)
+                moves.Add(Square.At(currentsquare.Row, i));
+
+            for (var i = 0; i < 8; i++)
+                moves.Add(Square.At(i, currentsquare.Col));
+            
+            return moves;
         }
     }
 }
