5d32bbf7632f8a5b4bc2c84fdd5e9b6083aa17b0
jknights2000 <56910576+jknights2000@users.noreply.github.com>
2021-12-01 16:57:25 +0000

    red-02
    pawn functionality and fix test

--- Chessington.GameEngine/Pieces/Pawn.cs
+++ Chessington.GameEngine/Pieces/Pawn.cs
@@ -10,17 +10,36 @@ namespace Chessington.GameEngine.Pieces
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
+            //1 for black 6 for white
+            int whitestart = 6;
+            int blackstart = 1;
             var currentsquare = board.FindPiece(this);
             List<Square> moves = new List<Square>();
-            if (this.Player == Player.White)
+      
+            if(this.Player == Player.White)
             {
-
-                moves.Add(new Square(currentsquare.Row - 1,currentsquare.Col));
-
+                if (currentsquare.Row == whitestart)
+                {
+                    moves.Add(new Square(currentsquare.Row - 1, currentsquare.Col));
+                    moves.Add(new Square(currentsquare.Row - 2, currentsquare.Col));
+                }
+                else
+                {
+                    moves.Add(new Square(currentsquare.Row - 1, currentsquare.Col));
+                }
+                
             }
-            else if (this.Player == Player.Black)
+            else
             {
-                moves.Add(new Square(currentsquare.Row + 1, currentsquare.Col));
+                if (currentsquare.Row == blackstart)
+                {
+                    moves.Add(new Square(currentsquare.Row + 1, currentsquare.Col));
+                    moves.Add(new Square(currentsquare.Row + 2, currentsquare.Col));
+                }
+                else
+                {
+                    moves.Add(new Square(currentsquare.Row + 1, currentsquare.Col));
+                }
             }
             return moves;
         }
